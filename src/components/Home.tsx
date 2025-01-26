6c8c1dbbc82a33490e1c1db7e8d2c9ad39f202b4
anurag2674 <g.anuragbobby@gmail.com>
2025-01-26 22:29:45 +0530

    Added input and button to enter location details

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import Button from '@mui/material/Button';
 const Home: React.FC<unknown> = (_props) => {
-  return <div>Home</div>;
+  return (
+    <div className="flex justify-center px-4 py-4">
+      <div className="flex items-start ">
+        <div className="flex gap-4 items-center">
+          <div>
+            <label htmlFor="location">Location: </label>
+            <input className="border border-black solid px-2 py-2" type="text" name="location" id="location" />
+          </div>
+          <div>
+            <Button variant="contained">Submit</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
